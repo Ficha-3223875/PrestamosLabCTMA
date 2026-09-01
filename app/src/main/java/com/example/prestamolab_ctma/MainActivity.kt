20bdd8a5e9464215be425e674f41d5cf141e562e
Lucciana Vergara <lucianavergaramartinez@gmail.com>
2026-09-01 10:42:35 -0500

    fix: corregir referencia del proyecto y configurar CI

--- app/src/main/java/com/example/prestamolab_ctma/MainActivity.kt
+++ app/src/main/java/com/example/prestamolab_ctma/MainActivity.kt
@@ -105,6 +105,7 @@ fun PrestamoLabApp(vm: PrestamoViewModel = viewModel()) {
     Scaffold(
         topBar = {
             TopAppBar(
+
                 title = { Text("PréstamoLab CTMA ANT", fontWeight = FontWeight.Bold) },
                 navigationIcon = {
                     if (screen !is AppScreen.Catalogo) {
